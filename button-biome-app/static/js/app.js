71fa4333e28c78b711c9b4f0ce1fa99961d9d181
Chuck <brian.vanhoff@gmail.com>
2019-08-08 20:11:44 -0400

    added guage chart and cleaned code

--- button-biome-app/static/js/app.js
+++ button-biome-app/static/js/app.js
@@ -1,92 +1,65 @@
 function buildMetadata(sample) {
-
-  // @TODO: Complete the following function that builds the metadata panel
-
-  // Use `d3.json` to fetch the metadata for a sample
-  var defaultURL = `/metadata/${sample}`;
-  d3.json(defaultURL).then(data => {
-
+  d3.json(`/metadata/${sample}`).then((data) => {
     // Use d3 to select the panel with id of `#sample-metadata`
-    var selection = d3.select(`#sample-metadata`);
+    var PANEL = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-    selection.html("");
+    PANEL.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
-    Object.entries(data).forEach(([key,value]) => {
-
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.  
-      selection.append('p')
-      .text(`${key}: ${value}`);
+    Object.entries(data).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key}: ${value}`);
     });
-  
-  })
-   // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+
+    // BONUS: Build the Gauge Chart
+    buildGauge(data.WFREQ);
+  });
 }
 
-
 function buildCharts(sample) {
+  d3.json(`/samples/${sample}`).then((data) => {
+    const otu_ids = data.otu_ids;
+    const otu_labels = data.otu_labels;
+    const sample_values = data.sample_values;
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-  var defaultURL = `/samples/${sample}`;
-  d3.json(defaultURL).then(data => {
+    // Build a Bubble Chart
+    var bubbleLayout = {
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" }
+    };
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+    ];
 
-    var ids = data.otu_ids;
-    var labels = data.otu_labels;
-    var counts = data.sample_values;
-    var dataObjectArray = [];
+    Plotly.plot("bubble", bubbleData, bubbleLayout);
 
-    // Combine the data sets into one array of objects (for sorting)
-    for (var d = 0; d < labels.length; d++) {
-      dataObjectArray.push({'count': counts[d], 
-      'code': ids[d], 'label': labels[d]}
-    )};
-    
-    // Sort the array in descending order according to the count key values
-    dataObjectArray.sort(function(a, b) {return (b.count) - (a.count)});
+    // Build a Pie Chart
+   var pieData = [
+      {
+        values: sample_values.slice(0, 10),
+        labels: otu_ids.slice(0, 10),
+        hovertext: otu_labels.slice(0, 10),
+        hoverinfo: "hovertext",
+        type: "pie"
+      }
+    ];
 
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-    var sorted_slice = dataObjectArray.slice(0,10);
+    var pieLayout = {
+      margin: { t: 0, l: 0 }
+    };
 
-    // @TODO: Build a Pie Chart using the sample data
-
-    var pieLayout = { title: "Relative Bacteria Type Percentages",
-                      indexLabelPlacement: "outside",
-                      "layout": "auto"
-                    };
-
-    var pieData = [{ 'values' : sorted_slice.map(data => data.count),
-                    'labels' : sorted_slice.map(data => data.code),
-                    'textinfo': 'percent',
-                    'hoverinfo' :('text'+'labels'+'values'+'percent'),
-                    'type' : 'pie'
-                  }];
-    
-    // Plot the pie chart
-    Plotly.newPlot("pie", pieData, pieLayout);
-
-    // @TODO: Build a Bubble Chart
-
-    var bubblelayout = {title: "Microbes Compared: (Displays All)",
-                        hovermode:"closest",
-                        xaxis: {title:"OTU ID (Microbe identification number)"},
-                        yaxis: {title:"Counts of microbe"}
-                      }; 
-      
-    var bubbledata=[{x:ids,
-                    y:counts,
-                    text: labels,
-                    mode: "markers",
-                    marker: {size:counts,
-                            color:ids,
-                            colorscale: "Earth"} 
-                    }];
-                    
-    // Plot the bubble chart                
-    Plotly.newPlot("bubble",bubbledata,bubblelayout);
+    Plotly.plot("pie", pieData, pieLayout);
   });
 }
 
@@ -102,7 +75,7 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-  
+
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
